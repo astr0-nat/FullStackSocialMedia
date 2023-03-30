9763ea41f4640cf381fff3a773b0db18aec15a1d
natalie pentschev <npentschev@gmail.com>
2023-03-30 13:10:24 -0700

    Revert "finished READ getFeedPosts and UPDATE likePost controllers for posts"
    This reverts commit 672c7d13b226c27b004f8237cfa48bee1d6b1eae.
    
    reorient the local git repo accordingly

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -5,8 +5,6 @@ import User from "../models/User.js";
 
 export const createPost = async (req, res) => {
   try {
-    /* req.body contains key-value pairs of data submitted in the request body. it is only populated
-    after using some parsing middleware such as body-parser or multer */
     const { userId, description, picturePath } = req.body;
     const user = await User.findById(userId);
     const newPost = new Post({
@@ -33,50 +31,5 @@ export const createPost = async (req, res) => {
     it's formatted */
   } catch (err) {
     res.status(409).json({ message: err.message });
-  }
-};
-
-/* READ */
-
-export const getFeedPosts = async (req, res) => {
-  try {
-    /* so here we are using req.params instead, as the userId will be a property in the object
-    provided by node that is maps named route "parameters" to properties like "/:userId"  */
-    const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
-
-/* UPDATE */
-// a lot of these routes require passing in updated values to the frontEnd so they can update the frontEnd feed
-export const likePost = async (req, res) => {
-  try {
-    const { id } = req.params; // we'll get the id of the post from the params
-    const { userId } = req.body; // we'll get the userId who is liking from the body here
-    const post = await Post.findById(id); // this id is the created by mongoDM, so they have a special method
-    // we check if user exists as true in this posts dict of likes, if so -click means unlike, if not - click means like
-    const isLiked = post.likes.get(userId);
-
-    if (isLiked) {
-      post.likes.delete(userId);
-    } else {
-      post.likes.set(userId, true);
-    }
-    /* options object that specifies for findByIdAndupdate
-     method to return the updated object instead of the originial 
-     object, which is the default */
-
-    const updatedPost = await Post.findByIdAndUpdate(
-      id,
-      { likes: post.likes },
-      { new: true }
-    );
-    // update the front end once clicked like button
-    res.status(200).json(updatedPost);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
   }
 };
