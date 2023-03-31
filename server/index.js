e7dea0c27379596e589c85476c56d14dc7a17fc3
natalie pentschev <npentschev@gmail.com>
2023-03-30 20:52:45 -0700

    fake user data added

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 import { createPost } from "./controllers/post.js"; // a controller we are going to make not
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js"
+
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -68,5 +72,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY. COMMENT OUT AFTER ONE USE */
+    User.insertMany(users);
+    Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
